0d34b03e2b3aef559c83852dab1bec8f21b0a44a
Navid2405 <lobatosaeznavidjose@gmail.com>
2025-07-04 11:28:15 -0500

    feat: Navid lobato- Ejemplos Taller 2

---
+++ src/Taller2/Ejemplos/Persona1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Taller2.Ejemplos;
+
+/**
+ *
+ * @author Navid Lobato
+ */
+public class Persona1 {
+
+    private String nombre;
+
+    public static void mostrarNombre() {
+       // System.out.println(this.nombre); // Error de compilación: No se puede usar `this` en un contexto estático
+    }
+}
